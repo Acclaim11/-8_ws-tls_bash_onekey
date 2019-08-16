b8608294b5505b79bdd6ebc1409849c51c73f0fe
Nopoint11 <50956970+Nopoint11@users.noreply.github.com>
2019-08-16 12:50:05 +0800

    Add files via upload

---
+++ corn11.sh
@@ -0,0 +1,192 @@
+#!/bin/bash
+
+#====================================================
+#	System Request:Debian 7+/Ubuntu 14.04+/Centos 6+
+#	Author:	wulabing
+#	Dscription: V2ray ws+tls onekey 
+#	Version: 3.3.1
+#	Blog: https://www.wulabing.com
+#	Official document: www.v2ray.com
+#====================================================
+
+#fonts color
+Green="\033[32m" 
+Red="\033[31m" 
+Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+Font="\033[0m"
+
+#notification information
+Info="${Green}[信息]${Font}"
+OK="${Green}[OK]${Font}"
+Error="${Red}[错误]${Font}"
+
+v2ray_conf_dir="/etc/v2ray"
+nginx_conf_dir="/etc/nginx/conf.d"
+v2ray_conf="${v2ray_conf_dir}/config.json"
+nginx_conf="${nginx_conf_dir}/v2ray.conf"
+
+#生成伪装路径
+camouflage=`cat /dev/urandom | head -n 10 | md5sum | head -c 8`
+
+source /etc/os-release
+
+#从VERSION中提取发行版系统的英文名称，为了在debian/ubuntu下添加相对应的Nginx apt源
+VERSION=`echo ${VERSION} | awk -F "[()]" '{print $2}'`
+
+check_system(){
+    
+    if [[ "${ID}" == "centos" && ${VERSION_ID} -ge 7 ]];then
+        echo -e "${OK} ${GreenBG} 当前系统为 Centos ${VERSION_ID} ${VERSION} ${Font} "
+        INS="yum"
+        echo -e "${OK} ${GreenBG} SElinux 设置中，请耐心等待，不要进行其他操作${Font} "
+        setsebool -P httpd_can_network_connect 1
+        echo -e "${OK} ${GreenBG} SElinux 设置完成 ${Font} "
+        ## Centos 也可以通过添加 epel 仓库来安装，目前不做改动
+        cat>/etc/yum.repos.d/nginx.repo<<EOF
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/mainline/centos/7/\$basearch/
+gpgcheck=0
+enabled=1
+EOF
+        echo -e "${OK} ${GreenBG} Nginx 源 安装完成 ${Font}" 
+    elif [[ "${ID}" == "debian" && ${VERSION_ID} -ge 8 ]];then
+        echo -e "${OK} ${GreenBG} 当前系统为 Debian ${VERSION_ID} ${VERSION} ${Font} "
+        INS="apt"
+        ## 添加 Nginx apt源
+        if [ ! -f nginx_signing.key ];then
+        echo "deb http://nginx.org/packages/mainline/debian/ ${VERSION} nginx" >> /etc/apt/sources.list
+        echo "deb-src http://nginx.org/packages/mainline/debian/ ${VERSION} nginx" >> /etc/apt/sources.list
+        wget -nc https://nginx.org/keys/nginx_signing.key
+        apt-key add nginx_signing.key
+        fi
+    elif [[ "${ID}" == "ubuntu" && `echo "${VERSION_ID}" | cut -d '.' -f1` -ge 16 ]];then
+        echo -e "${OK} ${GreenBG} 当前系统为 Ubuntu ${VERSION_ID} ${VERSION_CODENAME} ${Font} "
+        INS="apt"
+        ## 添加 Nginx apt源
+        if [ ! -f nginx_signing.key ];then
+        echo "deb http://nginx.org/packages/mainline/ubuntu/ ${VERSION_CODENAME} nginx" >> /etc/apt/sources.list
+        echo "deb-src http://nginx.org/packages/mainline/ubuntu/ ${VERSION_CODENAME} nginx" >> /etc/apt/sources.list
+        wget -nc https://nginx.org/keys/nginx_signing.key
+        apt-key add nginx_signing.key
+        fi
+    else
+        echo -e "${Error} ${RedBG} 当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断 ${Font} "
+        exit 1
+    fi
+
+}
+is_root(){
+    if [ `id -u` == 0 ]
+        then echo -e "${OK} ${GreenBG} 当前用户是root用户，进入安装流程 ${Font} "
+        sleep 3
+    else
+        echo -e "${Error} ${RedBG} 当前用户不是root用户，请切换到root用户后重新执行脚本 ${Font}" 
+        exit 1
+    fi
+}
+judge(){
+    if [[ $? -eq 0 ]];then
+        echo -e "${OK} ${GreenBG} $1 完成 ${Font}"
+        sleep 1
+    else
+        echo -e "${Error} ${RedBG} $1 失败${Font}"
+        exit 1
+    fi
+}
+ntpdate_install(){
+    if [[ "${ID}" == "centos" ]];then
+        ${INS} install ntpdate -y
+    else
+        ${INS} update
+        ${INS} install ntpdate -y
+    fi
+    judge "安装 NTPdate 时间同步服务 "
+}
+time_modify(){
+
+    ntpdate_install
+
+    systemctl stop ntp &>/dev/null
+
+    echo -e "${Info} ${GreenBG} 正在进行时间同步 ${Font}"
+    ntpdate time.nist.gov
+
+    if [[ $? -eq 0 ]];then 
+        echo -e "${OK} ${GreenBG} 时间同步成功 ${Font}"
+        echo -e "${OK} ${GreenBG} 当前系统时间 `date -R`（请注意时区间时间换算，换算后时间误差应为三分钟以内）${Font}"
+        sleep 1
+    else
+        echo -e "${Error} ${RedBG} 时间同步失败，请检查ntpdate服务是否正常工作 ${Font}"
+    fi 
+}
+start_process_systemd(){
+    ### nginx服务在安装完成后会自动启动。需要通过restart或reload重新加载配置
+    systemctl start nginx 
+    judge "Nginx 启动"
+
+
+    systemctl start v2ray
+    judge "V2ray 启动"
+}
+
+acme_cron_update(){
+    if [[ "${ID}" == "centos" ]];then
+        sed -i "/acme.sh/c 0 0 * * 0 systemctl stop nginx && \"/root/.acme.sh\"/acme.sh --cron --home \"/root/.acme.sh\" \
+        > /dev/null && systemctl start nginx " /var/spool/cron/root
+    else
+        sed -i "/acme.sh/c 0 0 * * 0 systemctl stop nginx && \"/root/.acme.sh\"/acme.sh --cron --home \"/root/.acme.sh\" \
+        > /dev/null && systemctl start nginx " /var/spool/cron/crontabs/root
+    fi
+    judge "cron 计划任务更新"
+}
+show_information(){
+    clear
+
+    echo -e "${OK} ${Green} V2ray+ws+tls 安装成功 "
+    echo -e "${Red} V2ray 配置信息 ${Font}"
+    echo -e "${Red} 地址（address）:${Font} ${domain} "
+    echo -e "${Red} 端口（port）：${Font} ${port} "
+    echo -e "${Red} 用户id（UUID）：${Font} ${UUID}"
+    echo -e "${Red} 额外id（alterId）：${Font} ${alterID}"
+    echo -e "${Red} 加密方式（security）：${Font} 自适应 "
+    echo -e "${Red} 传输协议（network）：${Font} ws "
+    echo -e "${Red} 伪装类型（type）：${Font} none "
+    echo -e "${Red} 路径（不要落下/）：${Font} /${camouflage}/ "
+    echo -e "${Red} 底层传输安全：${Font} tls "
+
+    
+
+}
+
+main(){
+    is_root
+    check_system
+    time_modify
+    dependency_install
+    domain_check
+    port_alterid_set
+    v2ray_install
+    port_exist_check 80
+    port_exist_check ${port}
+    nginx_install
+    v2ray_conf_add
+    nginx_conf_add
+    web_camouflage
+
+    #改变证书安装位置，防止端口冲突关闭相关应用
+    systemctl stop nginx
+    systemctl stop v2ray
+    
+    #将证书生成放在最后，尽量避免多次尝试脚本从而造成的多次证书申请
+    ssl_install
+    acme
+    
+    show_information
+    start_process_systemd
+    acme_cron_update
+}
+
+main

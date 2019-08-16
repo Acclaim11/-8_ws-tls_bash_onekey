c477f81065e0057a6cf3a648155b5283cbbc7128
Nopoint11 <50956970+Nopoint11@users.noreply.github.com>
2019-08-16 12:38:11 +0800

    Update cron111.sh

--- cron111.sh
+++ cron111.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-#====================================================
-#	System Request:Debian 7+/Ubuntu 14.04+/Centos 6+
-#	Author:	wulabing
-#	Dscription: V2ray ws+tls onekey 
-#	Version: 3.3.1
-#	Blog: https://www.wulabing.com
-#	Official document: www.v2ray.com
-#====================================================
-
 #fonts color
 Green="\033[32m" 
 Red="\033[31m" 
@@ -26,56 +15,6 @@ v2ray_conf_dir="/etc/v2ray"
 nginx_conf_dir="/etc/nginx/conf.d"
 v2ray_conf="${v2ray_conf_dir}/config.json"
 nginx_conf="${nginx_conf_dir}/v2ray.conf"
-
-#生成伪装路径
-camouflage=`cat /dev/urandom | head -n 10 | md5sum | head -c 8`
-
-source /etc/os-release
-
-#从VERSION中提取发行版系统的英文名称，为了在debian/ubuntu下添加相对应的Nginx apt源
-VERSION=`echo ${VERSION} | awk -F "[()]" '{print $2}'`
-
-check_system(){
-    
-    if [[ "${ID}" == "centos" && ${VERSION_ID} -ge 7 ]];then
-        echo -e "${OK} ${GreenBG} 当前系统为 Centos ${VERSION_ID} ${VERSION} ${Font} "
-        INS="yum"
-        echo -e "${OK} ${GreenBG} SElinux 设置中，请耐心等待，不要进行其他操作${Font} "
-        setsebool -P httpd_can_network_connect 1
-        echo -e "${OK} ${GreenBG} SElinux 设置完成 ${Font} "
-        ## Centos 也可以通过添加 epel 仓库来安装，目前不做改动
-        cat>/etc/yum.repos.d/nginx.repo<<EOF
-[nginx]
-name=nginx repo
-baseurl=http://nginx.org/packages/mainline/centos/7/\$basearch/
-gpgcheck=0
-enabled=1
-EOF
-        echo -e "${OK} ${GreenBG} Nginx 源 安装完成 ${Font}" 
-    elif [[ "${ID}" == "debian" && ${VERSION_ID} -ge 8 ]];then
-        echo -e "${OK} ${GreenBG} 当前系统为 Debian ${VERSION_ID} ${VERSION} ${Font} "
-        INS="apt"
-        ## 添加 Nginx apt源
-        if [ ! -f nginx_signing.key ];then
-        echo "deb http://nginx.org/packages/mainline/debian/ ${VERSION} nginx" >> /etc/apt/sources.list
-        echo "deb-src http://nginx.org/packages/mainline/debian/ ${VERSION} nginx" >> /etc/apt/sources.list
-        wget -nc https://nginx.org/keys/nginx_signing.key
-        apt-key add nginx_signing.key
-        fi
-    elif [[ "${ID}" == "ubuntu" && `echo "${VERSION_ID}" | cut -d '.' -f1` -ge 16 ]];then
-        echo -e "${OK} ${GreenBG} 当前系统为 Ubuntu ${VERSION_ID} ${VERSION_CODENAME} ${Font} "
-        INS="apt"
-        ## 添加 Nginx apt源
-        if [ ! -f nginx_signing.key ];then
-        echo "deb http://nginx.org/packages/mainline/ubuntu/ ${VERSION_CODENAME} nginx" >> /etc/apt/sources.list
-        echo "deb-src http://nginx.org/packages/mainline/ubuntu/ ${VERSION_CODENAME} nginx" >> /etc/apt/sources.list
-        wget -nc https://nginx.org/keys/nginx_signing.key
-        apt-key add nginx_signing.key
-        fi
-    else
-        echo -e "${Error} ${RedBG} 当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断 ${Font} "
-        exit 1
-    fi
 
 }
 acme_cron_update(){

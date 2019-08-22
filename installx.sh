e6f7114acb13202729c8646be16c8e436904665d
Nopoint11 <50956970+Nopoint11@users.noreply.github.com>
2019-08-22 22:15:34 +0800

    Update installx.sh

--- installx.sh
+++ installx.sh
@@ -24,11 +24,7 @@ Error="${Red}[错误]${Font}"
 
 v2ray_conf_dir="/etc/v2ray"
 nginx_conf_dir="/etc/nginx/conf.d"
-v2ray_conf="${v2ray_conf_dir}/config.json"
 nginx_conf="${nginx_conf_dir}/v2ray.conf"
-
-#生成伪装路径
-camouflage=`cat /dev/urandom | head -n 10 | md5sum | head -c 8`
 
 source /etc/os-release
 
@@ -142,20 +138,6 @@ dependency_install(){
     ${INS} install unzip -y
     judge "安装 unzip"
 }
-port_alterid_set(){
-    stty erase '^H' && read -p "请输入连接端口（default:443）:" port
-    [[ -z ${port} ]] && port="443"
-    stty erase '^H' && read -p "请输入alterID（default:64）:" alterID
-    [[ -z ${alterID} ]] && alterID="64"
-}
-modify_port_UUID(){
-    let PORT=$RANDOM+10000
-    UUID=$(cat /proc/sys/kernel/random/uuid)
-    sed -i "/\"port\"/c  \    \"port\":${PORT}," ${v2ray_conf}
-    sed -i "/\"id\"/c \\\t  \"id\":\"${UUID}\"," ${v2ray_conf}
-    sed -i "/\"alterId\"/c \\\t  \"alterId\":${alterID}" ${v2ray_conf}
-    sed -i "/\"path\"/c \\\t  \"path\":\"\/${camouflage}\/\"" ${v2ray_conf}
-}
 modify_nginx(){
     ## sed 部分地方 适应新配置修正
     if [[ -f /etc/nginx/nginx.conf.bak ]];then
@@ -164,33 +146,9 @@ modify_nginx(){
     sed -i "1,/listen/{s/listen 443 ssl;/listen ${port} ssl;/}" ${nginx_conf}
     sed -i "/server_name/c \\\tserver_name ${domain};" ${nginx_conf}
     sed -i "/location/c \\\tlocation \/${camouflage}\/" ${nginx_conf}
-    sed -i "/proxy_pass/c \\\tproxy_pass http://127.0.0.1:${PORT};" ${nginx_conf}
+    sed -i "/proxy_pass/c \\\tproxy_pass http://127.0.0.1:8888;" ${nginx_conf}
     sed -i "/return/c \\\treturn 301 https://${domain}\$request_uri;" ${nginx_conf}
     sed -i "27i \\\tproxy_intercept_errors on;"  /etc/nginx/nginx.conf
-}
-web_camouflage(){
-    ##请注意 这里和LNMP脚本的默认路径冲突，千万不要在安装了LNMP的环境下使用本脚本，否则后果自负
-    rm -rf /home/wwwroot && mkdir -p /home/wwwroot && cd /home/wwwroot
-    git clone https://github.com/Nopoint11/sCalc.git
-    judge "web 站点伪装"   
-}
-v2ray_install(){
-    if [[ -d /root/v2ray ]];then
-        rm -rf /root/v2ray
-    fi
-
-    mkdir -p /root/v2ray && cd /root/v2ray
-    wget  --no-check-certificate https://install.direct/go.sh
-
-    ## wget http://install.direct/go.sh
-    
-    if [[ -f go.sh ]];then
-        bash go.sh --force
-        judge "安装 V2ray"
-    else
-        echo -e "${Error} ${RedBG} V2ray 安装文件下载失败，请检查下载地址是否可用 ${Font}"
-        exit 4
-    fi
 }
 nginx_install(){
     ${INS} install nginx -y
@@ -274,12 +232,6 @@ acme(){
         exit 1
     fi
 }
-v2ray_conf_add(){
-    cd /etc/v2ray
-    wget https://raw.githubusercontent.com/Nopoint11/-8_ws-tls_bash_onekey/master/tls/config.json -O config.json
-modify_port_UUID
-judge "V2ray 配置修改"
-}
 nginx_conf_add(){
     touch ${nginx_conf_dir}/v2ray.conf
     cat>${nginx_conf_dir}/v2ray.conf<<EOF
@@ -291,13 +243,11 @@ nginx_conf_add(){
         ssl_protocols         TLSv1 TLSv1.1 TLSv1.2;
         ssl_ciphers           HIGH:!aNULL:!MD5;
         server_name           serveraddr.com;
-        index index.html index.htm;
-        root  /home/wwwroot/sCalc;
         error_page 400 = /400.html;
         location /ray/ 
         {
         proxy_redirect off;
-        proxy_pass http://127.0.0.1:10000;
+        proxy_pass http://127.0.0.1:8888;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
@@ -305,8 +255,8 @@ nginx_conf_add(){
         }
 }
     server {
-        listen 80;
-        server_name serveraddr.com;
+        listen 80 443;
+        server_name www.fizzeleven.com;
         return 301 https://use.shadowsocksr.win\$request_uri;
     }
 EOF
@@ -337,7 +287,6 @@ acme_cron_update(){
     judge "cron 计划任务更新"
 }
 show_information(){
-    clear
 
     echo -e "${OK} ${Green} V2ray+ws+tls 安装成功 "
     echo -e "${Red} V2ray 配置信息 ${Font}"

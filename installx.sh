ce48066a39964c9cfcc117516cc9901323684bef
Nopoint11 <50956970+Nopoint11@users.noreply.github.com>
2019-08-23 16:41:46 +0800

    Update and rename install.sh to installx.sh

--- installx.sh
+++ installx.sh
@@ -24,11 +24,7 @@ Error="${Red}[错误]${Font}"
 
 v2ray_conf_dir="/etc/v2ray"
 nginx_conf_dir="/etc/nginx/conf.d"
-v2ray_conf="${v2ray_conf_dir}/config.json"
 nginx_conf="${nginx_conf_dir}/v2ray.conf"
-
-#生成伪装路径
-camouflage=`cat /dev/urandom | head -n 10 | md5sum | head -c 8`
 
 source /etc/os-release
 
@@ -142,71 +138,6 @@ dependency_install(){
     ${INS} install unzip -y
     judge "安装 unzip"
 }
-port_alterid_set(){
-    stty erase '^H' && read -p "请输入连接端口（default:443）:" port
-    [[ -z ${port} ]] && port="443"
-    stty erase '^H' && read -p "请输入alterID（default:64）:" alterID
-    [[ -z ${alterID} ]] && alterID="64"
-}
-modify_port_UUID(){
-    let PORT=$RANDOM+10000
-    UUID=$(cat /proc/sys/kernel/random/uuid)
-    sed -i "/\"port\"/c  \    \"port\":${PORT}," ${v2ray_conf}
-    sed -i "/\"id\"/c \\\t  \"id\":\"${UUID}\"," ${v2ray_conf}
-    sed -i "/\"alterId\"/c \\\t  \"alterId\":${alterID}" ${v2ray_conf}
-    sed -i "/\"path\"/c \\\t  \"path\":\"\/${camouflage}\/\"" ${v2ray_conf}
-}
-modify_nginx(){
-    ## sed 部分地方 适应新配置修正
-    if [[ -f /etc/nginx/nginx.conf.bak ]];then
-        cp /etc/nginx/nginx.conf.bak /etc/nginx/nginx.conf
-    fi
-    sed -i "1,/listen/{s/listen 443 ssl;/listen ${port} ssl;/}" ${nginx_conf}
-    sed -i "/server_name/c \\\tserver_name ${domain};" ${nginx_conf}
-    sed -i "/location/c \\\tlocation \/${camouflage}\/" ${nginx_conf}
-    sed -i "/proxy_pass/c \\\tproxy_pass http://127.0.0.1:${PORT};" ${nginx_conf}
-    sed -i "/return/c \\\treturn 301 https://${domain}\$request_uri;" ${nginx_conf}
-    sed -i "27i \\\tproxy_intercept_errors on;"  /etc/nginx/nginx.conf
-}
-web_camouflage(){
-    ##请注意 这里和LNMP脚本的默认路径冲突，千万不要在安装了LNMP的环境下使用本脚本，否则后果自负
-    rm -rf /home/wwwroot && mkdir -p /home/wwwroot && cd /home/wwwroot
-    git clone https://github.com/Nopoint11/sCalc.git
-    judge "web 站点伪装"   
-}
-v2ray_install(){
-    if [[ -d /root/v2ray ]];then
-        rm -rf /root/v2ray
-    fi
-
-    mkdir -p /root/v2ray && cd /root/v2ray
-    wget  --no-check-certificate https://install.direct/go.sh
-
-    ## wget http://install.direct/go.sh
-    
-    if [[ -f go.sh ]];then
-        bash go.sh --force
-        judge "安装 V2ray"
-    else
-        echo -e "${Error} ${RedBG} V2ray 安装文件下载失败，请检查下载地址是否可用 ${Font}"
-        exit 4
-    fi
-}
-nginx_install(){
-    ${INS} install nginx -y
-    if [[ -d /etc/nginx ]];then
-        echo -e "${OK} ${GreenBG} nginx 安装完成 ${Font}"
-        sleep 2
-    else
-        echo -e "${Error} ${RedBG} nginx 安装失败 ${Font}"
-        exit 5
-    fi
-    if [[ ! -f /etc/nginx/nginx.conf.bak ]];then
-        cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-        echo -e "${OK} ${GreenBG} nginx 初始配置备份完成 ${Font}"
-        sleep 1
-    fi
-}
 ssl_install(){
     if [[ "${ID}" == "centos" ]];then
         ${INS} install socat nc -y        
@@ -274,57 +205,6 @@ acme(){
         exit 1
     fi
 }
-v2ray_conf_add(){
-    cd /etc/v2ray
-    wget https://raw.githubusercontent.com/Nopoint11/-8_ws-tls_bash_onekey/master/tls/config.json -O config.json
-modify_port_UUID
-judge "V2ray 配置修改"
-}
-nginx_conf_add(){
-    touch ${nginx_conf_dir}/v2ray.conf
-    cat>${nginx_conf_dir}/v2ray.conf<<EOF
-    server {
-        listen 443 ssl;
-        ssl on;
-        ssl_certificate       /etc/v2ray/v2ray.crt;
-        ssl_certificate_key   /etc/v2ray/v2ray.key;
-        ssl_protocols         TLSv1 TLSv1.1 TLSv1.2;
-        ssl_ciphers           HIGH:!aNULL:!MD5;
-        server_name           serveraddr.com;
-        index index.html index.htm;
-        root  /home/wwwroot/sCalc;
-        error_page 400 = /400.html;
-        location /ray/ 
-        {
-        proxy_redirect off;
-        proxy_pass http://127.0.0.1:10000;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$http_host;
-        }
-}
-    server {
-        listen 80;
-        server_name serveraddr.com;
-        return 301 https://use.shadowsocksr.win\$request_uri;
-    }
-EOF
-
-modify_nginx
-judge "Nginx 配置修改"
-
-}
-
-start_process_systemd(){
-    ### nginx服务在安装完成后会自动启动。需要通过restart或reload重新加载配置
-    systemctl start nginx 
-    judge "Nginx 启动"
-
-
-    systemctl start v2ray
-    judge "V2ray 启动"
-}
 
 acme_cron_update(){
     if [[ "${ID}" == "centos" ]];then
@@ -336,24 +216,6 @@ acme_cron_update(){
     fi
     judge "cron 计划任务更新"
 }
-show_information(){
-    clear
-
-    echo -e "${OK} ${Green} V2ray+ws+tls 安装成功 "
-    echo -e "${Red} V2ray 配置信息 ${Font}"
-    echo -e "${Red} 地址（address）:${Font} ${domain} "
-    echo -e "${Red} 端口（port）：${Font} ${port} "
-    echo -e "${Red} 用户id（UUID）：${Font} ${UUID}"
-    echo -e "${Red} 额外id（alterId）：${Font} ${alterID}"
-    echo -e "${Red} 加密方式（security）：${Font} 自适应 "
-    echo -e "${Red} 传输协议（network）：${Font} ws "
-    echo -e "${Red} 伪装类型（type）：${Font} none "
-    echo -e "${Red} 路径（不要落下/）：${Font} /${camouflage}/ "
-    echo -e "${Red} 底层传输安全：${Font} tls "
-
-    
-
-}
 
 main(){
     is_root
@@ -361,14 +223,8 @@ main(){
     time_modify
     dependency_install
     domain_check
-    port_alterid_set
-    v2ray_install
     port_exist_check 80
     port_exist_check ${port}
-    nginx_install
-    v2ray_conf_add
-    nginx_conf_add
-    web_camouflage
 
     #改变证书安装位置，防止端口冲突关闭相关应用
     systemctl stop nginx
@@ -378,7 +234,6 @@ main(){
     ssl_install
     acme
     
-    show_information
     start_process_systemd
     acme_cron_update
 }
